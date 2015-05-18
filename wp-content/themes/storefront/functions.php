0062c2ab6de6b144acfd1b5ddbf4c90e70cda9ef
Jackie <wangejay@gmail.com>
2015-05-18 23:45:39 +0800

    remove admin bar

--- wp-content/themes/storefront/functions.php
+++ wp-content/themes/storefront/functions.php
@@ -14,3 +14,11 @@ require get_template_directory() . '/inc/init.php';
  * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
  * http://codex.wordpress.org/Child_Themes
  */
+
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}

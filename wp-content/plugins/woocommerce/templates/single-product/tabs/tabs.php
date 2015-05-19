b199be48aa70a1147abd265ce10ecf0455068daa
Jackie <wangejay@gmail.com>
2015-05-19 18:14:10 +0800

    remove tab from single product

--- wp-content/plugins/woocommerce/templates/single-product/tabs/tabs.php
+++ wp-content/plugins/woocommerce/templates/single-product/tabs/tabs.php
@@ -22,15 +22,7 @@ $tabs = apply_filters( 'woocommerce_product_tabs', array() );
 if ( ! empty( $tabs ) ) : ?>
 
 	<div class="woocommerce-tabs">
-		<ul class="tabs">
-			<?php foreach ( $tabs as $key => $tab ) : ?>
-
-				<li class="<?php echo $key ?>_tab">
-					<a href="#tab-<?php echo $key ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', $tab['title'], $key ) ?></a>
-				</li>
-
-			<?php endforeach; ?>
-		</ul>
+		
 		<?php foreach ( $tabs as $key => $tab ) : ?>
 
 			<div class="panel entry-content" id="tab-<?php echo $key ?>">
